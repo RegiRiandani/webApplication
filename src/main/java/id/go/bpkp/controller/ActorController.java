07dc1acf1f45f7d1637ce1450f24d92bc5a6c0ef
Regi Riandani <Regi Riandani@RegiRiandani-PC>
2017-12-15 16:41:19 +0700

    commit-3

--- src/main/java/id/go/bpkp/controller/ActorController.java
+++ src/main/java/id/go/bpkp/controller/ActorController.java
@@ -1,14 +1,20 @@
 package id.go.bpkp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import id.go.bpkp.dao.ActorDAO;
 import id.go.bpkp.dao.FilmDAO;
+import id.go.bpkp.entity.Actor;
+import id.go.bpkp.entity.Film;
 import id.go.bpkp.entity.FilmActor;
 
 @Controller
@@ -27,6 +33,14 @@ public class ActorController {
 		model.addAttribute("actors",actorDAO.allActors());
 		return "actor/index";
 	}
+	@GetMapping("/indexFilm")
+	public String indexFilm(Model model) {
+		String bahasa[]= {"Melayu", "Tagalog","Thailand"};
+		model.addAttribute("negara", "Indonesia");
+		model.addAttribute("bahasa", bahasa);
+		model.addAttribute("films",actorDAO.allFilms());
+		return "actor/indexFilm";
+	}
 	@GetMapping("/detail/{id}")
 	public String detail(@PathVariable("id") int actorId, Model model)
 	{
@@ -43,6 +57,55 @@ public class ActorController {
 		model.addAttribute("Actors", actorDAO.allActorinFilm(filmId));
 		//model.addAttribute("film", "Indonesia");
 		return "actor/detailFilm";
+	}
+	
+	@GetMapping("/add")
+	public String add(Model model)
+	{
+		model.addAttribute("actor",new Actor());
+		return "actor/add";
+	}
+
+	@PostMapping("/add")
+	public String add(@Valid Actor actor,BindingResult result)
+	{
+		if(!result.hasErrors())
+		{
+				actorDAO.addActor(actor);
+		return "redirect:/actor/index";
+		}
+		else
+		{
+			return "actor/add";
+		}
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") short id,Model model)
+	{
+		model.addAttribute("actor",actorDAO.getActors(id));
+		return "actor/edit";
+	}
+
+	@PostMapping("/edit")
+	public String edit(Actor actor)
+	{
+		actorDAO.editActor(actor);
+		return "redirect:/actor/index";
+	}
+
+	@GetMapping("/addFilm")
+	public String addFilm(Model model)
+	{
+		model.addAttribute("film",new Film());
+		return "actor/addFilm";
+	}
+
+	@PostMapping("/addFilm")
+	public String addFilm(Film film)
+	{
+		actorDAO.addFilm(film);
+		return "redirect:/actor/indexFilm";
 	}
 
 }
--- src/main/java/id/go/bpkp/dao/ActorDAO.java
+++ src/main/java/id/go/bpkp/dao/ActorDAO.java
@@ -1,11 +1,17 @@
 package id.go.bpkp.dao;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import id.go.bpkp.entity.Actor;
 import id.go.bpkp.entity.Film;
@@ -23,6 +29,9 @@ public class ActorDAO {
 	 public List<Actor> allActors(){
 	    	return emf  .createEntityManager().createQuery("from Actor").getResultList();
 	    }
+	 public List<Film> allFilms(){
+	    	return emf  .createEntityManager().createQuery("from Film").getResultList();
+	    }
 	 public Actor getActors(int id){
 	    	return (Actor) emf  .createEntityManager().createQuery("from Actor where actorId=" + id).getSingleResult();
 	    	
@@ -38,5 +47,77 @@ public class ActorDAO {
 	    	return (Film) emf  .createEntityManager().createQuery("from Film where filmId=" + id).getSingleResult();
 	    	
 	    }
+	 public int addActor(Actor newActor)
+	 {
+		 EntityManager em = emf.createEntityManager();
+		 EntityTransaction trans = em.getTransaction();
+		 int hasil=0;
+		 try {
+			 trans.begin();
+			 newActor.setLastUpdate(new Date());
+			 em.persist(newActor);
+			 trans.commit();
+		 }
+		 catch(Exception ex) {
+			 trans.rollback();
+			 hasil=-1;
+			 System.out.println(ex.getMessage());
+		 }
+		 
+		 
+		 
+		 return hasil;
+		 
+	 }
+	 
+	 
+	   public Actor getActors(Short id){
+	   	return emf  .createEntityManager().find(Actor.class,id);
+	   }
+	 
+	   public int editActor(Actor actor) {//@RequestParam("actor") Actor) {
+		   int hasil=0;
+		   try {
+		   
+		   EntityManager e = emf.createEntityManager();
+		   e.getTransaction().begin();
+		   Actor currentActor=e.find(Actor.class, actor.getActorId());
+		   currentActor.setFirstName(actor.getFirstName());
+		   currentActor.setLastName(actor.getLastName());
+		   currentActor.setLastUpdate(new Date());
+		      
+		   e.getTransaction().commit();
+		   
+		   }
+		   catch(Exception ex) {
+			   System.out.println(ex.getMessage());
+			   hasil=-1;
+		   }
+		   
+		   return hasil;
+	   }
+	 
+	 public int addFilm(Film newFilm)
+	 {
+		 EntityManager em = emf.createEntityManager();
+		 EntityTransaction trans = em.getTransaction();
+		 int hasil=0;
+		 try {
+			 trans.begin();
+			 newFilm.setLastUpdate(new Date());
+			 em.persist(newFilm);
+			 trans.commit();
+		 }
+		 catch(Exception ex) {
+			 trans.rollback();
+			 hasil=-1;
+			 System.out.println(ex.getMessage());
+		 }
+		 
+		 
+		 
+		 return hasil;
+		 
+	 }
 	 
 }
